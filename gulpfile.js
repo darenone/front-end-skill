27033d44d7c653a7d244d941bbdd5613d35f1909
Zongq <1291342008@qq.com>
2017-03-07 15:21:12 +0800

    第一次把本地项目上传到GitHub上，好紧张哦

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var connect = require('gulp-connect');
+
+gulp.task('less',function(){
+	gulp.src('./src/less/*.less')
+		.pipe(less())
+		.pipe(gulp.dest('./src/css/'));
+});
+gulp.task('watchless',function(){
+	gulp.watch('./src/less/*.less',['less']);
+});
+
+gulp.task('connect',function(){
+	connect.server({
+		root: '../demo/',
+		livereload: true
+	});
+});
+
+gulp.task('watchHtml',function(){
+	gulp.watch(['./src/tpl/*.html'],['html']);
+});
+gulp.task('html',function(){
+	gulp.src('./src/tpl/*.html')
+		.pipe(connect.reload());
+});
+
+gulp.task('default',['connect','watchHtml','watchless'])
